8165e648c8ecde45234888c45c546e1f57971e24
hjs2148-gif <hjs2148@gmail.com>
2026-09-20 16:57:35 +0900

    Route Daily Day through root app

--- study flow/index.tsx
+++ study flow/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import DailyDay from './DailyDay';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+if (!rootElement) throw new Error("Could not find root element to mount to");
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+const params = new URLSearchParams(window.location.search);
+const showDailyDay = params.get('app') === 'dailyday';
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    {showDailyDay ? <DailyDay /> : <App />}
   </React.StrictMode>
 );
